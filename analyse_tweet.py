d37c01590a75c521e713bbcafb41ae9e909c4e04
Raphael D <dupuy.raphael01@gmail.com>
2022-11-29 16:10:18 +0100

    changes

--- analyse_tweet.py
+++ analyse_tweet.py
@@ -1,8 +1,13 @@
 import json
 
-# Opening Json file
-f = open('versailles_tweets_100.json', 'r', encoding="utf-8")
+def start_inpoda():
+    while 1:
+    
+        print("Bienvenue dans InPoDa")
+        f = open(input("Veuillez entrer la nom de votre fichier : ") + ".json", 'r', encoding="utf-8")
+        data = json.load(f)
+        
+        
 
-data = json.load(f)
 
-print(data[0])
+start_inpoda()
